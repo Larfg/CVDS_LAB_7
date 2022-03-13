58f80ed8bb4344b45d3386384153bad09e5633e5
Larfg <72176664+Larfg@users.noreply.github.com>
2022-03-12 19:49:48 -0500

    inicial

--- src/main/java/edu/eci/cvds/samples/services/client/MyBatisExample.java
+++ src/main/java/edu/eci/cvds/samples/services/client/MyBatisExample.java
@@ -25,6 +25,7 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import edu.eci.cvds.sampleprj.dao.mybatis.mappers.ClienteMapper;
 
 /**
  *
@@ -58,16 +59,12 @@ public class MyBatisExample {
      * @throws SQLException 
      */
     public static void main(String args[]) throws SQLException {
+
         SqlSessionFactory sessionfact = getSqlSessionFactory();
-
         SqlSession sqlss = sessionfact.openSession();
+        ClienteMapper cm = sqlss.getMapper(ClienteMapper.class);
+        System.out.println(cm.consultarClientes());
 
-        
-        //Crear el mapper y usarlo: 
-        //ClienteMapper cm=sqlss.getMapper(ClienteMapper.class)
-        //cm...
-        
-        
         
         sqlss.commit();
         
